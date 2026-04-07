5c9771c1682e16d3149d5ff08c7d16e0893f6323
Yasin Hirani <yasin.hirani@1rivet.com>
2026-04-07 18:47:29 +0530

    feat: added exact filter for stations

--- Repository/StationRepository.cs
+++ Repository/StationRepository.cs
@@ -13,7 +13,28 @@ namespace irctc.Repository
 
             var json = await File.ReadAllTextAsync(filePath);
 
-            var data = JsonSerializer.Deserialize<List<Station>>(json)?.Where(s => s.Name.Contains(query, StringComparison.CurrentCultureIgnoreCase) || s.Code.Contains(query, StringComparison.CurrentCultureIgnoreCase)).ToList();
+            var data = JsonSerializer.Deserialize<List<Station>>(json)
+            ?.Select(station => new
+            {
+                Station = station,
+                Score =
+                    station.Code.Equals(query, StringComparison.OrdinalIgnoreCase) ? 1 :
+                    station.Name.Equals(query, StringComparison.OrdinalIgnoreCase) ? 1 :
+
+                    station.Code.StartsWith(query, StringComparison.OrdinalIgnoreCase) ? 2 :
+                    station.Name.StartsWith(query, StringComparison.OrdinalIgnoreCase) ? 2 :
+
+                    station.Code.Contains(query, StringComparison.OrdinalIgnoreCase) ? 3 :
+                    station.Name.Contains(query, StringComparison.OrdinalIgnoreCase) ? 3 :
+
+                    4
+            })
+            .Where(s => s.Score < 4)
+            .OrderBy(s => s.Score)
+            .ThenBy(x => x.Station.Name)
+            .Select(s => s.Station)
+            .Take(20)
+            .ToList();
 
             return data ?? [];
         }
--- obj/Debug/net8.0/irctc.AssemblyInfo.cs
+++ obj/Debug/net8.0/irctc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("irctc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15cce99c69c6e4fa3ea0f63f18cb4475ab80a662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ab8876a646b297587f64fba861c831e2057b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("irctc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("irctc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
